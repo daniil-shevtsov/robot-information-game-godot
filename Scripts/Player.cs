48b6c159f51e5f84d7c4f92c93843a43f84c38a4
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2023-12-21 22:10:11 +0100

    forgot to add stuff

---
+++ Scripts/Player.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+    // How fast the player moves in meters per second.
+    [Export]
+    public int Speed { get; set; } = 14;
+
+    // The downward acceleration when in the air, in meters per second squared.
+    [Export]
+    public int FallAcceleration { get; set; } = 75;
+
+    private Vector3 _targetVelocity = Vector3.Zero;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var direction = Vector3.Zero;
+        // We check for each move input and update the direction accordingly.
+        if (Input.IsActionPressed("move_right"))
+        {
+            GD.Print("Move Right");
+            direction.X += 1.0f;
+        }
+        if (Input.IsActionPressed("move_left"))
+        {
+            direction.X -= 1.0f;
+        }
+        if (Input.IsActionPressed("move_back"))
+        {
+            // Notice how we are working with the vector's X and Z axes.
+            // In 3D, the XZ plane is the ground plane.
+            direction.Z += 1.0f;
+        }
+        if (Input.IsActionPressed("move_forward"))
+        {
+            direction.Z -= 1.0f;
+        }
+        if (direction != Vector3.Zero)
+        {
+            direction = direction.Normalized();
+            //GetNode<Node3D>("Pivot").LookAt(Position + direction, Vector3.Up);
+        }
+        // Ground velocity
+        _targetVelocity.X = direction.X * Speed;
+        _targetVelocity.Z = direction.Z * Speed;
+
+        // // Vertical velocity
+        // if (!IsOnFloor()) // If in the air, fall towards the floor. Literally gravity
+        // {
+        //     _targetVelocity.Y -= FallAcceleration * (float)delta;
+        // }
+
+        // Moving the character
+        Velocity = _targetVelocity;
+        MoveAndSlide();
+    }
+}
