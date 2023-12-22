6d6390ef85d3bc41626506eb63a76d24432b5129
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2023-12-22 10:48:10 +0100

    create pivot for camera

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -13,8 +13,13 @@ public partial class Player : CharacterBody3D
 
     private Vector3 _targetVelocity = Vector3.Zero;
 
+    private Camera3D camera = null;
+
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+    public override void _Ready()
+    {
+        camera = GetNode<Camera3D>("Camera3D");
+    }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) { }
